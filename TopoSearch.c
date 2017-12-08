201db0d5fe67df12fab8228b006e2483cc63230d
AlopexH2O <Alopex_zh@126.com>
2017-12-08 23:26:20 +0800

    Changes to be committed: modified:   TopoSearch.c

--- TopoSearch.c
+++ TopoSearch.c
@@ -103,7 +103,25 @@ void  TopoSearchModule(TopoSearch *Owner)
 {
     Uint16 temp16;
     int16 i, j, num_KG_checked;
-    Uint8 index;
+    Uint8 index, index_other, ifrom, iend;
+    Uint8 flg;
+
+    //检查设置是否准确
+    flg = 0x0;
+    if((Owner->Node_num > MAX_NODE)||(Owner->Conn_num > MAX_CONN))//开关节点个数设置越界
+        flg |= 0x1;
+    //开关两端节点设置有问题
+    for(i = 0;i < Owner->Conn_num;i++){
+        if(Owner->From_Conn[i] >= Owner->Node_num)
+            flg |= 0x1;
+        if(Owner->End_Conn[i] >= Owner->Node_num)
+            flg |= 0x1;
+    }
+    //很失望，竟然设置有问题，不用干活啦，放假
+    if(flg != 0x0){
+        Owner->flg_seterr = flg;
+        return;
+    }
 
     //初始化清空计算量
     for (i = 0; i < MAX_NODE; ++i){
@@ -118,12 +136,14 @@ void  TopoSearchModule(TopoSearch *Owner)
     for(i = 0;i < MAX_CONN;i++){
         if((*Owner->inRun[i] != 0x0) && (*Owner->inFlt[i] == 0x0)){
             Owner->isConnected[i] = 0x1;
+        }else{
+            Owner->isConnected[i] = 0x0;
         }
     }
+    //开始干活啦，伙计们
     Owner->pHead = 0;
     Owner->pEnd = 0;
-    num_KG_checked = 0;//开关遍历的个数
-    for(i = 0;i < MAX_NODE;i++){
+    for(i = 0;i < Owner->Node_num;i++){
         if(Owner->type[i] != 0x2) continue;//非主网并网点，pass
         //检查是否已经遍历过
         if(Owner->visited_Node[i] != 0) continue;//很遗憾，这个节点已经遍历过了
@@ -136,19 +156,17 @@ void  TopoSearchModule(TopoSearch *Owner)
             Owner->visited_Node[index] = 0x1;//已经不单纯了
             pHead = (pHead + 1) % MAX_NODE;//准星瞄准下一个目标
             //根据该点寻找其他相连节点
-            for(j = 0;j < MAX_CONN;j++){
-                if(isConnected[j] != 0) continue;//不好意思，这个开关已经被占领
-                
-            }
-
-
-
-            for(j = 0;j < MAX_NODE;j++){
-                if((temp16 & (0x1 << j)) == 0x0) continue;//该节点都没联系
-                if(Owner->TPNode[j].visited != 0x0) continue;//很遗憾，这个老乡已经被打劫过了
-                //hahah 找到了个单纯的，赶紧收入囊中
-                Owner->stack[pEnd] = j;
+            for(j = 0;j < Owner->Conn_num;j++){
+                if(Owner->isConnected[j] == 0) continue;//不好意思，这个开关已经跳开了
+                if(Owner->visited_Conn[j] != 0) continue;//不好意思，这个开关已经被打劫过了
+                ifrom = Owner->From_Conn[j];
+                iend = Owner->End_Conn[j];
+                if((ifrom != index) && (iend != index)) continue;//这个开关两侧都不是想要的，不要意思啊
+                (ifrom == index) ? index_other = iend : index_other = ifrom;
+                //万事具备
+                Owner->stack[pEnd] = index_other;//开关另一端压入
                 pEnd = (pEnd + 1) % MAX_NODE;
+                Owner->visited_Conn[j] = 0x1;//开关遍历标志置上
             }
         }
     }
@@ -156,14 +174,14 @@ void  TopoSearchModule(TopoSearch *Owner)
     Owner->Act = 0x0;
     for(i = 0;i < MAX_NODE;i++){
         Owner->Stat_[i] = Owner->Stat[i];//先看看上次一有没有遍历到
-        Owner->Stat[i] = Owner->TPNode[i].visited;//被皇军拜访过得，肯定是要交皇粮的。
+        Owner->Stat[i] = Owner->visited_Node[i];//被皇军拜访过得，肯定是要交皇粮的。
         //如果是突然变化的，肯定是个受害者
         if((Owner->Stat_[i] == 0x1) && (Owner->Stat[i] == 0x0)){
             Owner->Act[i] = 0x1;//变化了就置跳开标志，但只有一个计算点哎，过两天俺再想想
         } else {
             Owner->Act[i] = 0x0;
         }
-        Owner->Act |= Owner->Act[i];//置总的标志啊
+        Owner->Act |= Owner->Act[i];//置总的标志
     }
 
     
@@ -173,5 +191,5 @@ void  TopoSearchModule(TopoSearch *Owner)
 
 
 
-
+    return;
 }
